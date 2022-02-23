126a3c42a58a5550c8d258ddf106c804bd6e624d
Molinari, Matteo <matteo.molinari@adesso.ch>
2022-02-23 08:35:23 +0100

    Added tests for pipes

--- src/app/pipes/highlight-text.pipe.spec.ts
+++ src/app/pipes/highlight-text.pipe.spec.ts
@@ -1,8 +1,26 @@
 import { HighlightTextPipe } from './highlight-text.pipe';
 
 describe('HighlightTextPipe', () => {
+  const pipe = new HighlightTextPipe();
+
   it('create an instance', () => {
     const pipe = new HighlightTextPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('wrap a simple string', () => {
+    expect(pipe.transform('foobar', 'foo')).toBe('<span class="highlighted">foo</span>bar');
+  });
+
+  it('wrap an uppercase string', () => {
+    expect(pipe.transform('FOOBAR', 'foo')).toBe('<span class="highlighted">FOO</span>BAR');
+  });
+
+  it('wrap an string with spaces', () => {
+    expect(pipe.transform('foo bar', 'foo')).toBe('<span class="highlighted">foo</span> bar');
+  });
+
+  it('wrap multiple strings', () => {
+    expect(pipe.transform('foofoo', 'foo')).toBe('<span class="highlighted">foo</span><span class="highlighted">foo</span>');
   });
 });
--- src/app/pipes/location-filter.pipe.spec.ts
+++ src/app/pipes/location-filter.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { LocationFilterPipe } from './location-filter.pipe';
 
 describe('LocationFilterPipe', () => {
+  const pipe = new LocationFilterPipe();
+
   it('create an instance', () => {
-    const pipe = new LocationFilterPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should not filter', () => {
+    expect(pipe.transform([{name: "", locationId: 0}, {name: "", locationId: 0}])).toHaveSize(2);
+    expect(pipe.transform(null, 1)).toBeNull();
+  });
+
+  it('should filter by location', () => {
+    expect(pipe.transform([{name: "", locationId: 1}, {name: "", locationId: 0}], 1)).toHaveSize(1);
+  })
+
 });
